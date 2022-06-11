6077d87a03ac5c407f63c13cc96d2105add0a080
Janis <76683226+saschazesiger@users.noreply.github.com>
2022-06-12 01:32:38 +0200

    First complete Dump

--- creator/normalizer.py
+++ creator/normalizer.py
@@ -1,5 +1,6 @@
 from unicodedata import decimal
 from flask import Flask, render_template, flash, request, url_for, redirect, jsonify
+from datetime import timedelta
 from flask_mysqldb import MySQL
 from sqlalchemy import func, update, desc
 from os import path
@@ -19,25 +20,32 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-with open('./creator/ipfull.txt', 'r', encoding='utf-8') as f:
-    ips = f.read().split('\n')
 
-class var():
-    clock = 0
-    print = 0
-    array = []
+
 
 @app.route('/')
 def create():
+    with open('./creator/ipfull.txt', 'r', encoding='utf-8') as f:
+        ips = f.read().split('\n')
+    app.permanent_session_lifetime = timedelta(minutes=60)
+    print('START')
     cur = mysql.connection.cursor()
+    num = 0
+    clock = 0
     for ip in ips:
         array = ip.split(',')
         try:
-            cur.execute(f'''UPDATE v4 set lon = {array[1]}, lat = {array[2]}, region = {array[3]}, city = {array[4]}, zip = {array[5]} ''')
-            mysql.connection.commit()
+            cur.execute(f'''UPDATE v4 set lon = "{array[1]}", lat = "{array[2]}", region = "{array[3]}", city = "{array[4]}", zip = "{array[5]}" where ip_start = "{array[0]}"''')
             print(f"lon = {array[1]}, lat = {array[2]}, region = {array[3]}, city = {array[4]}, zip = {array[5]}")
         except Exception as e:
-            print(e)
+                print(f'ERROR: {e} at {array}')
+                break
+        if num > clock:
+            clock = clock + 1000
+            print(clock)
+            mysql.connection.commit()
+        num = num + 1
+    mysql.connection.commit()
     return f"Fertig"
 
 
